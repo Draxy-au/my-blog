4453a889b3a9cab423cf62f559186ae94b625671
William Hamilton <draxy80@gmail.com>
2021-11-16 03:22:03 +1000

    final client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticleListPage from './pages/ArticleListPage';
+import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div id="page-body">
+            <Routes>
+              <Route exact path='/' element={<HomePage />} />
+              <Route path='/about' element={<AboutPage />} />
+              <Route path='/article/:name' element={<ArticlePage />} />
+              <Route path='/articles-list' element={<ArticleListPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
